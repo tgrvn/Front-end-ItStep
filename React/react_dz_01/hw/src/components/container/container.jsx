6ae7715fe0d2681cf4979c3d474e6917e605fe77
tgrvn <94200506+tgrvn@users.noreply.github.com>
2022-04-20 09:20:41 +0300

    in work...

--- React/react_dz_01/hw/src/components/container/container.jsx
+++ React/react_dz_01/hw/src/components/container/container.jsx
@@ -1,10 +1,28 @@
-import { Bio } from '../biography/bio';
-import './style.scss';
+import { Bio } from '../biography/bio'
+import './style.scss'
 
 export function Container() {
-    return (
-        <div className='container'>
-            <Bio />
-        </div>
-    );
+  const [bio, setBio] = useState([
+    {
+      name: 'William Shakespeare',
+      country: 'England',
+      city: 'Stratford-upon-Avon',
+      born: '26 april 1564',
+      died: '23 April 1616 (aged 52)',
+      spouses: {
+        spouse: 'Anne Hathaway',
+      },
+      children: {
+        child: 'Susanna Hall',
+        child: 'Hamnet Shakespeare',
+        child: 'Judith Quiney',
+      },
+    },
+  ])
+
+  return (
+    <div className='container'>
+      <Bio />
+    </div>
+  )
 }
